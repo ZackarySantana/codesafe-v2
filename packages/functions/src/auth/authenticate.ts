228e2f3c63511323737b3912d3fcb5217c7c7f82
Zackary Santana <zackzackyack@gmail.com>
2023-07-04 20:50:20 -0400

    Dependency cleanup

--- packages/functions/src/auth/authenticate.ts
+++ packages/functions/src/auth/authenticate.ts
@@ -1,6 +1,22 @@
+import { ConnectToDatabase } from "@lunar/core/database";
 import { ApiHandler } from "sst/node/api";
 
+const db = ConnectToDatabase();
+
 export const handler = ApiHandler(async (_evt) => {
+    const user = await db
+        .selectFrom("person")
+        .selectAll()
+        .where("email", "=", "zackzackyack@gmail.com")
+        .execute();
+
+    if (user.length === 0) {
+        return {
+            statusCode: 404,
+            body: "User not found",
+        };
+    }
+
     return {
         statusCode: 200,
         body: `Hello world. The time is ${new Date().toISOString()}`,
