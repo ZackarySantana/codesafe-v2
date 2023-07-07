7f546c0b47eee4e739fb3d02e9081e8dede6b6cb
Zackary Santana <zackzackyack@gmail.com>
2023-07-06 21:30:08 -0400

    Bug fix

--- packages/core/src/validate/utils.ts
+++ packages/core/src/validate/utils.ts
@@ -19,6 +19,7 @@ export function validateProperties<T extends string>(
                 },
             ];
         }
+        obj[property] = test[property];
     }
     return [obj, undefined];
 }
